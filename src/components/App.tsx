11d415cd72b80fbac2cec707bd64685817bb21b7
DimaM83 <150599187+DimaM83@users.noreply.github.com>
2025-05-14 14:15:01 +0200

    добавил структуру файлов

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,6 @@
-import Product from "./Product";
+import Product from "./Product/product";
+import Mailbox from "./Mailbox/mailbox";
+
 
 export default function App() {
   return (
@@ -7,13 +9,22 @@ export default function App() {
       <Product
         name="Tacos With Lime"
         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
-      price={10.99}
+        price={10.99}
       />
+
       <Product
         name="Fries and Burger"
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
         price={14.29}
       />
-      </>
+
+
+      <Mailbox
+        username="Jacob"
+        messages={["Message 1", "Message 2", "Message 3", "Message 4"]}
+      />
+    </>
   );
 }
+
+
